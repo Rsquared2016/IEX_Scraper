5c6deb7f7919eadf197769a3ee185a6ec54245c1
Michael Hernandez <hernandez@uchicago.edu>
2017-06-15 17:50:08 -0500

    Some changes to layout of setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@ from distutils.core import setup
 setup(
     name='IEX_Scraper',
     version='0.1dev',
-    packages=['scripts'],
+    packages=['scripts','data'],
     url='',
     license='MIT',
     author='Michael Hernandez',
     author_email='',
     description='A tool for pulling data from the IEX exchange API.',
-    install_requires='requests'
+    install_requires='requests',
+    scripts="main.py",
+    package_data={
+        'data': ["*.db"]
+    },
+    include_package_data=True,
+    zip_safe=False
 )
 
